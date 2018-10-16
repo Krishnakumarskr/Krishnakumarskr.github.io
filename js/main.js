a8b105a936ea6d177c7a7cbb504feb6f1cabb10a
krishnakumar <krishna291098@gmail.com>
2018-10-17 00:41:50 +0530

    copied from local to github

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+(function($){
+    $(function(){
+  
+      $('.sidenav').sidenav();
+      $('.parallax').parallax();
+  
+    }); // end of document ready
+  })(jQuery); // end of jQuery name space
+//Scroll
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+      scrollTop: $($.attr(this, 'href')).offset().top
+  }, 500);
+});
