c20eb1a850e63ac4e2c672713f0e281ca96a1733
krishnakumar <krishna291098@gmail.com>
2018-10-18 04:26:21 +0530

    added skills

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-
 (function($){
-    $(function(){
-  
-      $('.sidenav').sidenav();
-      $('.parallax').parallax();
-  
-    }); // end of document ready
-  })(jQuery); // end of jQuery name space
+  $(function(){
+
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
 //Scroll
 $(document).on('click', 'a[href^="#"]', function (event) {
   event.preventDefault();
@@ -15,3 +14,20 @@ $(document).on('click', 'a[href^="#"]', function (event) {
       scrollTop: $($.attr(this, 'href')).offset().top
   }, 500);
 });
+
+
+var skillsDiv = jQuery('#skill');
+
+jQuery(window).on('scroll', function() {
+  var winT = jQuery(window).scrollTop(),
+    winH = jQuery(window).height(),
+    skillsT = skillsDiv.offset().top;
+  if (winT +winH > skillsT) {
+    jQuery('.skillbar').each(function() {
+      jQuery(this).find('.skillbar-bar').animate({
+        width: jQuery(this).attr('data-percent')
+      }, 2500);
+    });
+  }
+});
+
