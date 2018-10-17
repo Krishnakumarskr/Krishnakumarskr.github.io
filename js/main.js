d68f39b1c00fef14991f7f675529282151add7b0
Krishna kumar S <krishna291098@gmail.com>
2018-10-17 22:48:05 +0530

    testing sidenav
    deleted the scroll jquery.

--- js/main.js
+++ js/main.js
@@ -8,10 +8,10 @@
     }); // end of document ready
   })(jQuery); // end of jQuery name space
 //Scroll
-$(document).on('click', 'a[href^="#"]', function (event) {
+/*$(document).on('click', 'a[href^="#"]', function (event) {
   event.preventDefault();
 
   $('html, body').animate({
       scrollTop: $($.attr(this, 'href')).offset().top
   }, 500);
-});
+});*?
