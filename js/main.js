c9ccfc30c58c3c671ade4b7083cb9bd37f62973f
krishnakumar <krishna291098@gmail.com>
2018-10-20 22:17:52 +0530

    completed pending divs

--- js/main.js
+++ js/main.js
@@ -1,33 +1,43 @@
+
+// Scroll
+// $(document).on('click', 'a[href^="#"]', function (event) {
+//   event.preventDefault();
+
+//   $('html, body').animate({
+//       scrollTop: $($.attr(this, 'href')).offset().top
+//   }, 500);
+// });
+
+$('a[href*=#]').click(function(event){
+  $('html, body').animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+  }, 500);
+  event.preventDefault();
+});
+
+
+// var skillsDiv = $('#skill');
+
+// $(window).on('scroll', function(){
+// 	var winT = $(window).scrollTop(),
+//   	winH = $(window).height(),
+//   	skillsT = skillsDiv.offset().top;
+//   if(winT + winH  > skillsT){
+//   	$('.skillbar').each(function(){
+//       $(this).find('.skillbar-bar').animate({
+//         width:$(this).attr('data-percent')
+//       },2500);
+//       return true;
+//     });
+//   }
+// });
+
+
 (function($){
   $(function(){
 
     $('.sidenav').sidenav();
-    $('.parallax').parallax();
+   
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
-//Scroll
-$(document).on('click', 'a[href^="#"]', function (event) {
-  event.preventDefault();
-
-  $('html, body').animate({
-      scrollTop: $($.attr(this, 'href')).offset().top
-  }, 500);
-});
-
-
-var skillsDiv = jQuery('#skill');
-
-jQuery(window).on('scroll', function() {
-  var winT = jQuery(window).scrollTop(),
-    winH = jQuery(window).height(),
-    skillsT = skillsDiv.offset().top;
-  if (winT +winH > skillsT) {
-    jQuery('.skillbar').each(function() {
-      jQuery(this).find('.skillbar-bar').animate({
-        width: jQuery(this).attr('data-percent')
-      }, 2500);
-    });
-  }
-});
-
